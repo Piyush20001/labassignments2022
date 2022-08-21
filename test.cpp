7c59179aeabe905beed71345cb3d52b34d76cfd8
Piyush20001 <44575274+Piyush20001@users.noreply.github.com>
2022-08-21 10:35:34 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -9,13 +9,13 @@ struct node {
 struct node *head = NULL;
 struct node *current = NULL;
 
-//display the list
+
 void printList() {
 
    struct node *ptr = head;
 
    printf("\n[head] =>");
-   //start from the beginning
+
    while(ptr != NULL) {        
       printf(" %d =>",ptr->data);
       ptr = ptr->next;
@@ -24,18 +24,17 @@ void printList() {
    printf(" [null]\n");
 }
 
-//insert link at the first location
+
 void insert(int data) {
    //create a link
    struct node *link = (struct node*) malloc(sizeof(struct node));
 
-   //link->key = key;
+
    link->data = data;
 
-   //point it to old first node
+ 
    link->next = head;
 
-   //point first to new first node
    head = link;
 }
 
